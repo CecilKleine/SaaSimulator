ad4ac63a1c66bf94d0e57bd44d4cb8b4aad51a33
Cecil Kleine <cecil@Cecils-MacBook-Air.local>
2025-12-15 22:07:05 +0100

    Configure for GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/startup-game-2',
+  assetPrefix: '/startup-game-2',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
